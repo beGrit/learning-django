73070c6e03b0ec4ae53c2364585aca63f7cdb9fe
Lin Shengfeng <1134187280@qq.com>
2022-03-14 22:23:33 +0800

    GP-0003: Home page init.

--- medical/views.py
+++ medical/views.py
@@ -10,7 +10,54 @@ from medical.models import Vaccination
 
 
 def home_page(request):
-    return TemplateResponse(request, 'custom/pages/home/home.html')
+    home_banner_items = [
+        {
+            'details_url_path': '',
+            'title': '查疾病',
+            'description': '不知道生啥病了？快来看看权威疾病百科',
+        },
+        {
+            'details_url_path': '',
+            'title': '查药品',
+            'description': '不知道吃啥药？快来看看药品信息大全',
+        },
+        {
+            'details_url_path': '',
+            'title': '查医院',
+            'description': '看看附近有哪些医院？',
+        },
+        {
+            'details_url_path': '',
+            'title': '查检查/手术',
+            'description': '检查与手术专业百科',
+        },
+        {
+            'details_url_path': '',
+            'title': '查疫苗',
+            'description': '疫苗还有多少？哪些疫苗适合你呢？',
+        },
+        {
+            'details_url_path': '',
+            'title': '测一测',
+            'description': '你是否困惑自己是不是到底生病了没？这里有专业的试题，评估您的问题',
+        },
+    ]
+    popularization_articles = [
+        {
+            'facade_image_url_path': '',
+            'title': '',
+            'content': '',
+            'detail_url_path': '',
+        },
+    ]
+    return TemplateResponse(request,
+                            'custom/pages/home/home.html',
+                            context={
+                                'home_banner_items': home_banner_items,
+                                'home_article': {
+                                    'popularization_articles': popularization_articles
+                                },
+                            })
 
 
 def activity_vaccination_subscribe(request):
